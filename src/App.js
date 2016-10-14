68e2294ab97c44e599f3ee50c8e1189e450fa011
Jeremy <jcsetojseto@gmail.com>
2016-10-14 00:41:48 -0400

    quick update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ var App = React.createClass({
           <h1>Strava Deleter</h1>
           <h2> Connect to your Strava account to deleted unwanted Activities! </h2>
         </div>
-        <div id="main">
+        <div id="main"> 
           <a href="https://www.strava.com/oauth/authorize">
             <img src={stravaLogin}></img>
           </a>
