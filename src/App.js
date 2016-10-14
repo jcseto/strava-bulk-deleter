4f959e68479fc56d4637f35e32d531300778f6b1
jcseto <jcsetojseto@gmail.com>
2016-10-13 22:38:40 -0400

    Added basic Intro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import stravaLogin from './Assets/LoginWithStrava.png';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+
+var App = React.createClass({
+  render: function(){
+    return(
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Strava Deleter</h1>
+          <h2> Connect to your Strava account to deleted unwanted Activities! </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="main">
+          <a href="https://www.strava.com/oauth/authorize">
+            <img src={stravaLogin}></img>
+          </a>
+        </div>
       </div>
-    );
+    )
   }
-}
+});
 
 export default App;
